8c16f0d53061957b6508a9ee9210bf3d70899945
graccius <129871386+graccius@users.noreply.github.com>
2024-03-07 02:20:53 -0300

    fibonnaci

---
+++ soma.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+  int x,y,z;
+  int i, t;
+scanf("%d",&t);
+if(t>0){
+for (i=0;i<t;i++){
+  x = 0;
+  y = z;
+  z = y;
+  x = x + 1;
+  printf("%d,"x);
+}
+}
+else{
+break}
+}
